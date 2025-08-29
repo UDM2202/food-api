f097b118497a1283773b062949620e60f1909bc7
UDM2202 <118545652+UDM2202@users.noreply.github.com>
2025-08-28 18:52:19 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+import Meals from './Meals'
+
+function App() {
+  return (
+    <div>
+      {/* <Todo/> */}
+      <Meals />
+    </div>
+  )
+}
+
+export default App
